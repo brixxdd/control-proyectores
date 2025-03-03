174a542e0764009489f4361a169353290dc208b0
brixxdd <brixxdd@gmail.com>
2025-03-02 19:07:13 -0600

    .env finales

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    'https://control-proyectores-2wir.vercel.app'
+    'https://control-proyectores-unach.vercel.app'
   ],
   credentials: true,
 }));
